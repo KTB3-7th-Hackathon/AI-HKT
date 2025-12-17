1d2ac1a8d7bad03830a1b6dbe800362a8f23c2f7
Yun <mosy2266@naver.com>
2025-12-17 15:11:43 +0900

    Initial commit

---
+++ apps/backend/src/main/java/com/example/ktb3team7hktbe/Ktb3Team7HktBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ktb3team7hktbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ktb3Team7HktBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ktb3Team7HktBeApplication.class, args);
+    }
+
+}
---
+++ apps/backend/src/test/java/com/example/ktb3team7hktbe/Ktb3Team7HktBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ktb3team7hktbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ktb3Team7HktBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
